2ab4bccea5611b45b52fa85156b771fa123b6fe2
Henry <HenryCraft@gmx.de>
2022-05-05 14:45:52 +0200

    Switch to countUp.js

--- script/script.js
+++ script/script.js
@@ -1,4 +1,13 @@
-// Count on Scroll: https://github.com/pablog1/js-animated-counter
+// Count on Scroll: https://github.com/inorganik/countUp.js
+import { CountUp } from "./countUp.js";
+
+const options = {
+    duration: 5
+};
+
+let c1 = new CountUp("stat1counter", 432, options);
+let c2 = new CountUp("stat2counter", 864, options);
+let c3 = new CountUp("stat3counter", 432, options);
 
 // Hide navbar on scroll
 // Source: https://codingreflections.com/hide-header-on-scroll-down/
@@ -41,68 +50,34 @@
     window.addEventListener("scroll", checkScroll);
 })();
 
-// Scroll Counter
-// Source: https://marioyepes.com/pure-javascript-scroll-counter/
-/*
-document.addEventListener("DOMContentLoaded", function () {
-    // You can change this class to specify which elements are going to behave as counters.
-    var elements = document.querySelectorAll(".scroll-counter")
-
-    elements.forEach(function (item) {
-        // Add new attributes to the elements with the ".scroll-counter" HTML class
-        item.counterAlreadyFired = false
-        item.counterSpeed = item.getAttribute("data-counter-time") / 45
-        item.counterTarget = +item.innerText
-        item.counterCount = 0
-        item.counterStep = item.counterTarget / item.counterSpeed
-
-        item.updateCounter = function () {
-            item.counterCount = item.counterCount + item.counterStep
-            item.innerText = Math.ceil(item.counterCount)
-
-            if (item.counterCount < item.counterTarget) {
-                setTimeout(item.updateCounter, item.counterSpeed)
-            } else {
-                item.innerText = item.counterTarget
-            }
-        }
-    })
-
-    // Function to determine if an element is visible in the web page
-    var isElementVisible = function isElementVisible(el) {
-        var scroll = window.scrollY || window.pageYOffset
-        var boundsTop = el.getBoundingClientRect().top + scroll
-        var viewport = {
-            top: scroll,
-            bottom: scroll + window.innerHeight,
-        }
-        var bounds = {
-            top: boundsTop,
-            bottom: boundsTop + el.clientHeight,
-        }
-        return (
-            (bounds.bottom >= viewport.top && bounds.bottom <= viewport.bottom) ||
-            (bounds.top <= viewport.bottom && bounds.top >= viewport.top)
-        )
-    }
-
-    // Funciton that will get fired uppon scrolling
-    var handleScroll = function handleScroll() {
-        elements.forEach(function (item, id) {
-            if (true === item.counterAlreadyFired) return
-            if (!isElementVisible(item)) return
-            item.updateCounter()
-            item.counterAlreadyFired = true
-        })
-    }
-
-    // Fire the function on scroll
-    window.addEventListener("scroll", handleScroll)
-})
-*/
-
 // Waypoints (execute code on scroll)
 // Source: http://imakewebthings.com/waypoints/
+
+var counterPointTop = new Waypoint({
+    element: document.getElementById("counters_1"),
+    handler: function (direction) {
+        if (direction === "down") {
+            c1.start();
+            c2.start();
+            c3.start();
+        } else {
+            c1.reset();
+            c2.reset();
+            c3.reset();
+        }
+
+    }, offset: "98%"
+})
+
+var counterPointBottom = new Waypoint({
+    element: document.getElementById("counters_1"),
+    handler: function () {
+        c1.start();
+        c2.start();
+        c3.start();
+    }, offset: -100
+})
+
 var waypoint = new Waypoint({
     element: document.getElementById("start-first-animation"),
     handler: function (direction) {
@@ -116,6 +91,9 @@ var waypoint = new Waypoint({
             topContainer.classList.remove("stick-to-top")
             bottomContainer.classList.remove("stick-to-top")
         }
+        c1.reset();
+        c2.reset();
+        c3.reset();
     }
 })
 
@@ -123,13 +101,30 @@ var waypoint2 = new Waypoint({
     element: document.getElementById("start-second-animation"),
     handler: function (direction) {
         const topContainer = document.querySelector("#static-image");
-        const bottomContainer = document.querySelector("#animated-image");
-        console.log("triggered");
-        
+
         if (direction === "down") {
             topContainer.classList.add("hide-animation");
         } else {
             topContainer.classList.remove("hide-animation");
         }
+    }, offset: "-10%"
+})
+
+var waypoint3 = new Waypoint({
+    element: document.getElementById("end-first-animation"),
+    handler: function (direction) {
+        const topContainer = document.querySelector("#static-image");
+        const bottomContainer = document.querySelector("#animated-image");
+
+        if (direction === "down") {
+            topContainer.classList.add("stick-to-bottom");
+            bottomContainer.classList.add("stick-to-bottom");
+        } else {
+            topContainer.classList.remove("stick-to-bottom")
+            bottomContainer.classList.remove("stick-to-bottom")
+        }
+        document.getElementsByClassName("counter")[0].setAttribute("data-targetnum", 0)
+        document.getElementsByClassName("counter")[1].setAttribute("data-targetnum", 0)
+        document.getElementsByClassName("counter")[2].setAttribute("data-targetnum", 0)
     }
 })
